23b766273601b842fb5f9f0a1c609db9ff4b0055
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-06-19 10:09:59 +0300

    feat: solve task args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const arr = process.argv.slice(2);
+  let resultStr = "";
+
+  for (let i = 0; i < arr.length; i += 2) {
+    resultStr += `${arr[i].slice(2)} is ${arr[i + 1]}, `;
+  }
+  console.log(resultStr.slice(0, -2));
 };
 
 parseArgs();
