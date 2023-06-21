12bfcda8c0f347d4142621a800e2aa833b3e542b
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-06-21 13:23:57 +0300

    feat: solve read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { readFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const targetFolder = join(__dirname, "files");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const file = await readFile(join(targetFolder, "fileToRead.txt"), {
+      encoding: "utf8",
+    });
+    console.log(file);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    } else {
+      console.log(error);
+    }
+  }
 };
 
 await read();
