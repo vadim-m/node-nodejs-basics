5ee657f1d9ca9493bc3a29357ab0d1fd123478a1
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-06-21 13:14:46 +0300

    feat: solve delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { rm } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const targetFolder = join(__dirname, "files");
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await rm(join(targetFolder, "fileToRemove.txt"));
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
